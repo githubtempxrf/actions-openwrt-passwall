8e6fe013cb3b6d1043f50a30d5ad04b3df6baead
githubtempxrf <63188112+githubtempxrf@users.noreply.github.com>
2020-04-24 13:11:06 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,8 +6,11 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.100/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.20/g' package/base-files/files/bin/config_generate
 cd package/lean/  
-git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
+git clone --recursive https://github.com/jerrykuku/lua-maxminddb.git
+git clone --recursive https://github.com/jerrykuku/luci-app-vssr.git
+git clone --recursive https://github.com/vernesong/OpenClash.git
+mv OpenClash/luci-app-openclash .
 cd -
+ 
